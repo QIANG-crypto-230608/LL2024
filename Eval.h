5b90816c8e4fda80bab8261c89177e05ec75a594
Qiang_Crypto <115140250+QIANG-crypto-230608@users.noreply.github.com>
2024-12-26 18:20:57 +0900

    This code implements the protocol presented in the paper "Efficient Multi-party Private Set Union Resistant to Maximum Collusion Attacks."

---
+++ Eval.h
@@ -0,0 +1,22 @@
+//
+// Created by QIANG LIU on 12/3/24.
+//
+
+#ifndef EVAL_H
+#define EVAL_H
+
+#include <string>
+#include <cryptoTools/Common/TestCollection.h>
+#include <macoro/when_all.h>
+#include <macoro/sync_wait.h>
+#include <macoro/task.h>
+
+inline auto eval(macoro::task<>& t0, macoro::task<>& t1)
+{
+    auto r = macoro::sync_wait(macoro::when_all_ready(std::move(t0), std::move(t1)));
+    std::get<0>(r).result();
+    std::get<1>(r).result();
+}
+
+
+#endif
